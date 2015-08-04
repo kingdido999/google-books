9402d9fb4d523b7f0b4639cb066490f30794d31c
kingdido999 <kingdido999@gmail.com>
2015-08-04 15:31:12 -0400

    add timeout

--- src/GoogleBooks/GoogleBooks.php
+++ src/GoogleBooks/GoogleBooks.php
@@ -25,6 +25,8 @@ class GoogleBooks {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
         $data = curl_exec($ch);
         curl_close($ch);
 
